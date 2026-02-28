30591c203d95df46c6efa3968ab9f57e1e2c92c7
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 12:59:48 +0545

    Added signup

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('signup/', views.signup, name='signup'),
     path('get-response/', views.get_response, name='get_response'),
 ]
