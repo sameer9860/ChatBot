b1b4e798aab229170b332b9c3e9dbcb675a40ab0
Samir Khatiwada <samirkhatiwada68@gmail.com>
2026-02-28 12:56:52 +0545

    Added chat model

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default="New Chat")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sender = models.CharField(max_length=10)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
